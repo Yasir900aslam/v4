80b88717a9dfe49336489264293a0dd0c75834dd
yasir <yasir900aslam@gmail.com>
2021-01-18 05:35:54 +0500

    Base

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -48,12 +48,12 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Yasir Aslam.</h2>;
+  const three = <h3 className="big-heading">I am a Full-Stack Developer</h3>;
   const four = (
     <p>
-      I'm a software engineer based in Boston, MA specializing in building (and occasionally
-      designing) exceptional websites, applications, and everything in between.
+      I specialize in creating beautiful websites, CMS, PWA. I love to travel and sometime I play
+      football to stay sane.
     </p>
   );
   const five = (
